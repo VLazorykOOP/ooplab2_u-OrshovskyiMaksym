c9e65bb5caa8e9f1c01cb9fdc982585c94d7c0a1
OrshovskyiMaksym <112846338+OrshovskyiMaksym@users.noreply.github.com>
2022-10-30 13:33:12 +0200

    Task1

--- OOPLab2Tpl/Tasks.cpp
+++ OOPLab2Tpl/Tasks.cpp
@@ -17,6 +17,26 @@ void task1() {
     // Обчислення виразів з використанням побітових операцій
     // Calculation of expressions using bitwise operations 
     cout << " Calculation of expressions using bitwise operations  \n";
+	
+	int a, b, x, d, c, y;
+	int r, r1;
+	cout << "Input a: ";
+	cin >> a;
+	cout << "Input b: ";
+	cin >> b;
+	cout << "Input c: ";
+	cin >> c;
+	cout << "Input d: ";
+	cin >> d;
+
+
+	x = b + (b << 1) + (b << 1) + (b << 5) + (((d << 5) - d + (a << 3) + (a << 2)) >> 11) - ((c << 6) + c) + (d << 4) - (d << 1);
+
+	y = 37 * b + (((d * 31) + (12 * a)) / 2048) - (65 * c) + d * 14;
+
+	cout << "Result (without multiplication) = " << x << "\nResult = " << y << "\na=" << a << "; b=" << b << "; c=" << c << "; d=" << d << ";" << endl;
+	return 0;
+	
 }
 
 void task2()
